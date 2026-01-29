5afb6cbe48b204495e9fafafd8a0177a79dfe6a9
joeyCV93 <Joeycv@hotmail.co.uk>
2026-01-29 22:17:05 +0000

    SF-25121.7 Correct Sort by Price Asc and Desc to use UnitPrices to ensure correct lucene field is chosen

--- Ucommerce.Sitefinity.UI/Mvc/Model/ProductModel.cs
+++ Ucommerce.Sitefinity.UI/Mvc/Model/ProductModel.cs
@@ -101,21 +101,13 @@ namespace UCommerce.Sitefinity.UI.Mvc.Model
 
                     if (sortExpression == "PriceAsc")
                     {
-                        productsQuery = productsQuery.OrderBy(p =>
-                            CatalogLibrary.CalculatePrices(new List<Guid>
-                                        { p.Guid },
-                                    null)
-                                .Items.First()
-                                .ListPriceExclTax);
+                        var currencyCode = CatalogContext.CurrentPriceGroup.CurrencyISOCode;
+                        productsQuery = productsQuery.OrderBy(x => x.UnitPrices[currencyCode]);
                     }
                     else if (sortExpression == "PriceDesc")
                     {
-                        productsQuery = productsQuery.OrderByDescending(p =>
-                            CatalogLibrary.CalculatePrices(new List<Guid>
-                                        { p.Guid },
-                                    null)
-                                .Items.First()
-                                .ListPriceExclTax);
+                        var currencyCode = CatalogContext.CurrentPriceGroup.CurrencyISOCode;
+                        productsQuery = productsQuery.OrderByDescending(x => x.UnitPrices[currencyCode]);
                     }
                     else if (sortExpression == "NameAsc")
                     {
